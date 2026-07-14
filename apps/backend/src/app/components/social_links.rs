b904879257681466fced15c8421a010e7accea92
Ken Esparta <kenesparta@pm.me>
2026-07-13 20:12:40 -0500

    feat: create Social Link

--- apps/backend/src/app/components/social_links.rs
+++ apps/backend/src/app/components/social_links.rs
@@ -3,17 +3,32 @@ use leptos::prelude::*;
 use leptos::{component, view, IntoView};
 
 #[component]
+fn SocialLink(
+    #[prop(into)] href: String,
+    #[prop(into)] aria_label: String,
+    #[prop(optional, into)] download: Option<String>,
+    children: Children,
+) -> impl IntoView {
+    view! {
+        <a
+            href=href
+            target="_blank"
+            rel="noopener noreferrer"
+            download=download
+            class="social-links__element"
+            aria-label=aria_label
+        >
+            {children()}
+        </a>
+    }
+}
+
+#[component]
 pub fn SocialLinks() -> impl IntoView {
     let resume = format!("{}/cv/ken_esparta_cv.pdf", BUCKET_URL);
     view! {
         <div class="social-links">
-            <a
-                href="https://github.com/kenesparta"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="social-links__element"
-                aria-label="Visit my GitHub profile"
-            >
+            <SocialLink href="https://github.com/kenesparta" aria_label="Visit my GitHub profile">
                 <svg
                     width="30"
                     height="30"
@@ -23,32 +38,11 @@ pub fn SocialLinks() -> impl IntoView {
                 >
                     <path d="M12 0C5.374 0 0 5.373 0 12 0 17.302 3.438 21.8 8.207 23.387c.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.300 24 12c0-6.627-5.373-12-12-12z"/>
                 </svg>
-            </a>
+            </SocialLink>
 
-            <a
-                href="https://linkedin.com/in/kenesparta"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="social-links__element"
-                aria-label="Visit my LinkedIn profile"
-            >
-                <svg
-                    width="30"
-                    height="30"
-                    viewBox="0 0 30 30"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-                </svg>
-            </a>
-
-            <a
+            <SocialLink
                 href="https://codeberg.org/kenesparta"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="social-links__element"
-                aria-label="Visit my Codeberg profile"
+                aria_label="Visit my Codeberg profile"
             >
                 <svg
                     width="30"
@@ -59,14 +53,27 @@ pub fn SocialLinks() -> impl IntoView {
                 >
                     <path d="M11.999.747A11.974 11.974 0 0 0 0 12.75c0 2.254.635 4.465 1.833 6.376L11.837 6.19c.072-.092.251-.092.323 0l4.178 5.402h-2.992l.065.239h3.113l.882 1.138h-3.674l.103.374h3.86l.777 1.003h-4.358l.135.483h4.593l.695.894h-5.038l.165.589h5.326l.609.785h-5.717l.182.65h6.038l.562.727h-6.397l.183.65h6.717A12.003 12.003 0 0 0 24 12.75 11.977 11.977 0 0 0 11.999.747zm3.654 19.104.182.65h5.326c.173-.204.353-.433.513-.65zm.385 1.377.18.65h3.563c.233-.198.485-.428.712-.65zm.383 1.377.182.648h1.203c.356-.204.685-.412 1.042-.648z"/>
                 </svg>
-            </a>
+            </SocialLink>
 
-            <a
-                href={resume}
-                target="_blank"
+            <SocialLink
+                href="https://linkedin.com/in/kenesparta"
+                aria_label="Visit my LinkedIn profile"
+            >
+                <svg
+                    width="30"
+                    height="30"
+                    viewBox="0 0 30 30"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                </svg>
+            </SocialLink>
+
+            <SocialLink
+                href=resume
                 download="kenesparta-resume.pdf"
-                class="social-links__element"
-                aria-label="Download my Resume"
+                aria_label="Download my Resume"
             >
                 <svg
                     width="33"
@@ -77,8 +84,7 @@ pub fn SocialLinks() -> impl IntoView {
                 >
                     <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/>
                 </svg>
-            </a>
-
+            </SocialLink>
         </div>
     }
 }
